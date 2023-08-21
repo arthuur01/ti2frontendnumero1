b3af37d0de6949a50000224c1fd2a9ad9b3f89fe
Admin <Admin@DESKTOP-CUILBGF.mshome.net>
2023-08-21 15:45:12 -0300

    Primeiro Commit

---
+++ Soma/src/numeroum/soma.java
@@ -0,0 +1,18 @@
+package numeroum;
+
+
+import java.util.Scanner;
+
+public class soma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int soma = num1 + num2;
+		
+		System.out.println(soma);
+		sc.close();
+	}
+}
